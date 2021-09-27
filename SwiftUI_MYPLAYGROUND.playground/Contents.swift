d082a9223ce265da6cdd7ec1bb0920b87fd9e871
Michalis Papakostas <mpapakostas@gnhearing.com>
2021-09-27 16:05:07 -0500

    day one assignment complete

--- SwiftUI_MYPLAYGROUND.playground/Contents.swift
+++ SwiftUI_MYPLAYGROUND.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
 var greeting = "Hello, playground"
+greeting = "Byeeeeee"
+var age = 38
+var population = 8_000_000
+//greeting = 90 --> invalid, type missmatch
+
+var multilinestr = """
+    This goes
+    over multiple
+    lines
+    """
+
+var multilinestrCor = """
+    This goes \
+    over multiple \
+    lines
+    """
+
+var pi = 3.14 //double vaar
+var isBoolean = true // boolean var
+
+
+// Complex Strings
+var score = 85
+var complexStr = "Your score is \(score)" //string interpolations
+var moreComplexStr = "You are baad because: \(complexStr)"
+
+
+//Constants
+let taylor = "swift"
+//taylor = "john" //invalid operation
+
+
+
+//Type Annotations
+let str = "Automaticly recognized aas string"
+let year: Int = 1989 //specify as int
+let yearst: String = "1989" //specify as string
+let height: Double = 1.78 //specify as double
+let taylorRocks: Bool = true //specify as boolean
